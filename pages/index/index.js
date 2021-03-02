bd01c6c40404193f7312183ce3f705f08ed5b92f
lanqiukun <1543323033@qq.com>
2021-03-03 00:19:32 +0800

    asdf

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,6 +5,16 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
   },
 
+  check() {
+    wx.checkSession({
+      success () {
+        console.log('session_key 未过期，并且在本生命周期一直有效')
+      },
+      fail () {
+        console.log('session_key 已经失效，需要重新执行登录流程')
+      }
+    })
+  },
 
   bindGetUserInfo (e) {
 
@@ -19,7 +29,10 @@ Page({
               iv: e.detail.iv,
               encryptedData: e.detail.encryptedData
             },
-            method: "POST"
+            method: "POST",
+            success: response => {
+              console.log(response.data)
+            }
           })
         } else {
           console.log('登录失败！' + res.errMsg)
